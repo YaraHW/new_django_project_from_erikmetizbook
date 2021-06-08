6de2cb0a2a81970ab19d629a56231cb91932c482
yara <yaroslav.chepurnoy@bk.ru>
2021-06-09 02:56:35 +1100

    minor updates

--- new_django_project_from_erikmetizbook/settings.py
+++ new_django_project_from_erikmetizbook/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0twtpuw_*@y9d8w7%gxs)(%xm@gx5%twz!$&(389)6%0yq-_9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
