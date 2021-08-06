48a7fd283b5214b26a2ef66ed66046835886efd5
YaraHW <mister.chepurnoy@mail.ru>
2021-08-06 12:15:48 +1100

    DB changed to postgres Removed almost all from requirements.txt

--- new_django_project_from_erikmetizbook/settings.py
+++ new_django_project_from_erikmetizbook/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,17 +28,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
 
-    #Мои приложения
+    # Мои приложения
     "learning_logs",
-     'users', # для регистрации новых пользователей
+    'users',  # для регистрации новых пользователей
     # Сторонние приложения
     'bootstrap4',
-    #Приложения по умолчанию
+    # Приложения по умолчанию
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,14 +77,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'new_django_project_from_erikmetizbook.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '15432',
     }
 }
 
@@ -108,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -121,7 +122,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -137,7 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/users/login/'
 
 # Настройки Heroku
-import django_heroku
+
+
 django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
