c2d9553da74e77ea5cf1f44b7632e0b41534f2ab
YaraHW <mister.chepurnoy@mail.ru>
2021-08-07 11:12:53 +1100

    fixed problems with database

--- new_django_project_from_erikmetizbook/settings.py
+++ new_django_project_from_erikmetizbook/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'gbpltw13',
         'HOST': '127.0.0.1',
-        'PORT': '15432',
+        'PORT': '5432',
     }
 }
 
